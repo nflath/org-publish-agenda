e067eb0590eff49f7baf6ad04f3d25196ad77654
Nathaniel Flath <flat0103@gmail.com>
2014-10-03 10:07:54 -0700

    First commit of org-publish-agenda.el

---
+++ org-publish-agenda.el
@@ -0,0 +1,140 @@
+;;; org-publish-agenda.el --- Publish org agenda with links to other files
+
+;; Copyright (C) 2014 Nathaniel Flath <flat0103@gmail.com>
+
+;; Author: Nathaniel Flath <flat0103@gmail.com>
+;; Version: 1.2
+
+;; This file is not part of GNU Emacs.
+
+;;; Commentary:
+
+;; org-publish-agenda provides a function `org-publish-agenda` that will export
+;; every org file in (org-agenda-files).  It will then export the agenda,
+;; creating links between the items on the agenda and the sections in the other
+;; files.
+
+;;; Installation
+
+;; To use this mode, put the following in your init.el:
+;; (require 'org-publish-agenda)
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 3
+;; of the License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Code:
+
+(require 'org)
+(require 'htmlize)
+
+(defun org-publish-agenda-after-last (regexp string)
+  "Returns the part of the string after the last occurrence of regexp."
+  (let ((index (string-match regexp string)))
+    (if index
+        (org-publish-agenda-after-last regexp (substring string (match-end 0) (length string)))
+      string)))
+
+(defun org-publish-agenda-current-section-number (&optional pos)
+  "Returns the subsection number at pos"
+  (save-excursion
+    (if pos (goto-char pos))
+    (let ((retn 1))
+      (ignore-errors
+        (while t
+          (outline-backward-same-level 1)
+          (incf retn)))
+      retn)))
+
+(defun org-publish-agenda-full-sections (&optional pos)
+  "Returns a list coresponding to the full section number at pos"
+  (save-excursion
+    (if pos (goto-char pos))
+    (let* ((retn 1)
+           (curnum (org-publish-agenda-current-section-number))
+           (retlist (list curnum)))
+      (condition-case nil
+          (while t
+            (progn
+              (outline-up-heading 1)
+              (setq retlist (append (list (org-publish-agenda-current-section-number)) retlist))))
+        (error retlist))
+      retlist)))
+
+(defun org-publish-agenda-full-sections-string (&optional pos)
+  "Returns a string corresponding to the section at pos"
+  (substring (reduce (lambda (x y) (concat x "." (number-to-string y)))
+                     (org-publish-agenda-full-sections)
+                     :initial-value "") 1))
+
+(defun org-publish-agenda-line-matches (regexp)
+  "Returns non-nil if the current line matches the given regexp, nil otherwise."
+  (interactive "sRegex: ")
+  (save-excursion
+    (end-of-line)
+    (let ((end (point)))
+      (beginning-of-line)
+      (re-search-forward regexp end t))))
+
+(defun org-publish-agenda ()
+  "Writes out the agenda and all agenda files as HTML."
+  (interactive)
+  (save-window-excursion
+    (mapcar (lambda (file)
+              (find-file file)
+              (org-html-export-as-html)
+              (write-file (replace-regexp-in-string "\\.org" ".html" file))
+              (kill-buffer))
+            (org-agenda-files) ))
+  (org-agenda 0 "a")
+  (org-agenda-month-view)
+  (let ((html-buffer (htmlize-buffer (get-buffer "*Org Agenda*")))
+        (agenda-buffer (get-buffer "*Org Agenda*")))
+    (switch-to-buffer html-buffer)
+    (goto-char (point-min))
+    (search-forward "<body>")
+    (let ((line-start (line-number-at-pos)))
+      (while (< (point) (point-max))
+        (beginning-of-line)
+        (cond
+         ((org-publish-agenda-line-matches "org-agenda-structure") (forward-line))
+         ((org-publish-agenda-line-matches "org-agenda-dat") (forward-line))
+         ((org-publish-agenda-line-matches "org-time-grid") (forward-line))
+         ((org-publish-agenda-line-matches " *\\([^:]+\\):")
+          (let ((calendar (org-publish-agenda-after-last " "(match-string 1))))
+            (let ((agenda-line-no (1- (- (line-number-at-pos) line-start))))
+              (switch-to-buffer agenda-buffer)
+              (goto-line agenda-line-no)
+              (let* ((marker (or (get-text-property (point) 'org-marker)
+                                 (org-agenda-error)))
+                     (buffer (marker-buffer marker))
+                     (pos (marker-position marker)))
+                (switch-to-buffer buffer)
+                (goto-char pos)
+                (setq sec-string (concat "sec-" (org-publish-agenda-full-sections-string)))
+                (switch-to-buffer html-buffer)))
+            (insert (concat "<a href=\"" calendar ".html#" sec-string "\">"))
+            (end-of-line)
+            (insert "</a>")
+            (forward-line)))
+         (t (forward-line)))))
+    (write-file (concat (car org-agenda-files) "agenda.html"))
+    (let ((default-directory (car org-agenda-files)))
+      (shell-command "mv *.html publish/" nil)
+      (kill-buffer "agenda.html"))))
+
+(provide 'org-publish-agenda)
+;;; org-publish-agenda.el ends here
